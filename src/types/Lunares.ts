d4125a633ffabaa75ee0231fd7b46bce3a48a7b9
Marcin Baraniecki <marcin@swissborg.com>
2021-02-16 11:23:49 +0100

    tiles order adjusted

--- src/types/Lunares.ts
+++ src/types/Lunares.ts
@@ -1,7 +1,7 @@
 export type LunarAreaId = "A1" | "A2" | "A3" | "A4" | "B1" | "B2" | "B3" | "B4";
 
 export const AllLunarAreaIds: LunarAreaId[] = [
-  "A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4"
+  "A4", "A3", "A2", "A1", "B4", "B3", "B2", "B1"
 ];
 
 export type MapEvents = {
